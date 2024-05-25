9a54ddbdbc8ca2143aa8331901cd7654c7c0f1ce
thomaschiari <thomaschiari@gmail.com>
2024-05-25 03:17:27 -0300

    Add high score functionality to GameOverController

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -7,10 +7,25 @@ using UnityEngine.SceneManagement;
 public class GameOverController : MonoBehaviour
 {
     public TextMeshProUGUI scoreText; // Texto para exibir a pontuação
+    public TextMeshProUGUI highScoreText; // Texto para exibir a pontuação máxima
+    public TextMeshProUGUI newHighScore; // Texto para exibir o contador de minerais
 
     void Start()
     {
         scoreText.text = PlayerPrefs.GetFloat("Score").ToString();
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            highScoreText.text = PlayerPrefs.GetFloat("HighScore").ToString();
+        }
+        else
+        {
+            highScoreText.text = PlayerPrefs.GetFloat("Score").ToString();
+        }
+        if (PlayerPrefs.GetFloat("Score") > PlayerPrefs.GetFloat("HighScore"))
+        {
+            PlayerPrefs.SetFloat("HighScore", PlayerPrefs.GetFloat("Score"));
+            newHighScore.text = "New High Score!";
+        }
     }
 
     public void backToMenu()

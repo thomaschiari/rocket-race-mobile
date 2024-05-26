57f90ed8c19e76fb0f17e5b47c220115f8e68b96
thomaschiari <thomaschiari@gmail.com>
2024-05-25 23:26:02 -0300

    Add RandomizeMineral script and update SpriteManager

--- Assets/Scripts/SpriteManager.cs
+++ Assets/Scripts/SpriteManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SpriteManager : MonoBehaviour
 {
     public Sprite[] sprites; // Array de sprites
+    public Sprite[] minerals; // Array de minerais
 
     public Sprite GetRandomSprite()
     {
@@ -13,5 +14,16 @@ public class SpriteManager : MonoBehaviour
         }
         int index = Random.Range(0, sprites.Length);
         return sprites[index];
+    }
+
+    public Sprite GetRandomMineral()
+    {
+        if (minerals.Length == 0)
+        {
+            Debug.LogWarning("No minerals available in SpriteManager!");
+            return null;
+        }
+        int index = Random.Range(0, minerals.Length);
+        return minerals[index];
     }
 }

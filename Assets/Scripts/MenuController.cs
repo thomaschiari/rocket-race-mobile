0f6d1c9f5539b2b167453169d426823e9a3dad2c
Lucca Hiratsuca <luccahiratsuca@gmail.com>
2024-05-28 18:59:16 -0300

    feat: MenuController Script

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,12 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    public Button startButton;
+
+    void Awake()
+    {
+        PlayerPrefs.SetInt("GameMode", 0);
+        PlayerPrefs.SetInt("StoryMode", 0);
+    }
+
+    void Start()
+    {
+        UpdateStartButtonState();
+    }
+
+    void Update()
+    {
+        UpdateStartButtonState();
+    }
     public void StartGame()
     {
-        SceneManager.LoadScene("GameView");
+        if (PlayerPrefs.GetInt("GameMode") == 1)
+        {
+            SceneManager.LoadScene("GameView");
+        }
+        else if (PlayerPrefs.GetInt("StoryMode") == 1)
+        {
+            SceneManager.LoadScene("StoryGameView");
+        }
     }
 
     public void ChooseShip()
@@ -19,4 +44,29 @@ public class MenuController : MonoBehaviour
     // {
     //     SceneManager.LoadScene("Rules");
     // }
+
+    public void SelectEndlessMode()
+    {
+        PlayerPrefs.SetInt("GameMode", 1);
+        PlayerPrefs.SetInt("StoryMode", 0);
+    }
+
+    public void SelectStoryMode()
+    {
+        PlayerPrefs.SetInt("StoryMode", 1);
+        PlayerPrefs.SetInt("GameMode", 0);
+    }
+
+    private void UpdateStartButtonState()
+    {
+        if (PlayerPrefs.GetInt("GameMode") == 0 && PlayerPrefs.GetInt("StoryMode") == 0)
+        {
+            startButton.interactable = false;
+        }
+        else
+        {
+            startButton.interactable = true;
+        }
+    }
+
 }
